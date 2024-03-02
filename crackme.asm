674324fc3e2eefc1ee460b514bf2b2aa2e18b1a7
SeveratheDuck <antipov.aa@phystech.edu>
2024-03-02 12:11:08 +0300

    Corrected version of my crackme.asm

--- crackme.asm
+++ crackme.asm
@@ -6,52 +6,20 @@ org 100h
 
 
 ;----------------------------------------------------------
-;----------------------------------------------------------
-Start:
-;----------------------------------------------------------
-;----------------------------------------------------------
-
-
-
-;----------------------------------------------------------
-; Function prints phrase "Access granted" and ends program
-; Enter: none
-; Exit:  none
-; Destr: AX, DX
-;----------------------------------------------------------
-GrantAccess     proc
-
-                mov ah, 09                              ; DOS Fn 09h - Display text
-                mov dx, offset access_granted_str       ; DX -> access_granted_str
-                int 21h                                 ; print string
-
-                mov ax, 4c00h                           ; DOS Fn 4ch - Terminate
-                int 21h                                 ; Terminate
-
-                ret
-                endp
-;----------------------------------------------------------
+PASSWORD_LENGTH         equ 10          ; Length of password string
+ENTER_SCAN_CODE         equ 1ch         ;
+BACKSPACE_SCAN_CODE     equ 0eh         ;
+CIPHER_STEP             equ 1           ; 
 ;----------------------------------------------------------
 
 
 
 ;----------------------------------------------------------
-; Function prints phrase "Access denied" and ends program
-; Enter: none
-; Exit:  none
-; Destr: AX, DX
 ;----------------------------------------------------------
-DenyAccess      proc
+Start:          call ReadPassword
+                call CheckPassword
 
-                mov ah, 09                              ; DOS Fn 09h - Display text
-                mov dx, offset access_denied_str        ; DX -> access_denied_str
-                int 21h                                 ; print string
-
-                mov ax, 4c00h                           ; DOS Fn 4ch - Terminate
-                int 21h                                 ; Terminate
-
-                ret
-                endp
+                jmp ChooseAccess
 ;----------------------------------------------------------
 ;----------------------------------------------------------
 
@@ -67,34 +35,32 @@ DenyAccess      proc
 ;----------------------------------------------------------
 ReadPassword    proc
 
+                mov bx, offset typed_pwd
+
                 ;----------------------
+                ; Cycle to read letter and
+                ; put it into typed_password
+                ; until ENTER is pressed.
+                ; Press BACKSPACE to erase letter.
+                ;----------------------
+ReadLetter:     ;----------------------
                 ; Interrupts and BIOS services
                 ; AH = 00h
                 ; int 16h - read symbol
                 ;----------------------
-                xor ax, ax
                 mov ah, 00h
+                int 16h                         ; read letter from keyboard
 
-                xor bx, bx                      ; BX is a letter counter
-
-                ;----------------------
-                ; Cycle to read letter
-                ; and push it to stack
-                ; until ENTER is pressed.
-                ; Press BACKSPACE to pop letter.
-                ;----------------------
-ReadLetter:     int 16h                         ; read letter from keyboard
-                cmp al, ENTER_SCAN_CODE         ; if (AL == ENTER_SCAN_CODE)
+                cmp ah, ENTER_SCAN_CODE         ; if (AL == ENTER_SCAN_CODE)
                 je EndReadLetter                ;       EndReadLetter;
 
-                cmp al, BACKSPACE_SCAN_CODE     ; if (AL != BACKSPACE_SCAN_CODE)
-                jne PushLetter                  ;       PushLetter, BX++;
+                cmp ah, BACKSPACE_SCAN_CODE     ; if (AL != BACKSPACE_SCAN_CODE)
+                jne PutLetter                   ;       PutLetter, BX++;
 
-PopLetter:      pop ax                          ; else PopLetter, BX--;
-                dec bx
+EraseLetter:    dec bx                          ; else EraseLetter, BX--;
                 jmp ReadLetter
 
-PushLetter:     push ax
+PutLetter:      mov [bx], al
                 inc bx
                 jmp ReadLetter
 
@@ -105,21 +71,112 @@ EndReadLetter:  ret
 
 
 
+;----------------------------------------------------------
+;
+;
+;
+;
+;----------------------------------------------------------
+CheckPassword   proc
+
+                mov si, offset typed_pwd                ; SI -> typed password
+                mov di, offset reference_pwd            ; DI -> reference password
+
+                push cs
+                pop es                                  ; ES = CS
+
+                mov cx, PASSWORD_LENGTH                 ; CX = PASSWORD_LENGTH, counter
+
+DecipherSymbol: add byte ptr [si], CIPHER_STEP
+                inc si
+                loop DecipherSymbol
+                
+                sub si, PASSWORD_LENGTH
+                mov cx, PASSWORD_LENGTH
+
+                repe cmpsb
+
+                cmp cx, 0                               ; if (cx != 0)
+                jne EndCheckPassword                    ;       { EndCheckPassword; }
+                                                        ; else  { access_function_address -> GrantAccess; }
+                mov access_function_address, GrantAccess - ChooseAccess - 2
+
+EndCheckPassword:
+                ret
+                endp
+;----------------------------------------------------------
+;----------------------------------------------------------
 
 
 
-
-
+;----------------------------------------------------------
+; String to print if access is granted
 ;----------------------------------------------------------
 access_granted_str      db "ACCESS GRANTED$"
 ;----------------------------------------------------------
 
 ;----------------------------------------------------------
+; String to print if access is denied
+;----------------------------------------------------------
 access_denied_str       db "ACCESS DENIED$"
 ;----------------------------------------------------------
 
+
+
 ;----------------------------------------------------------
-reference_pwd           db "0123456789"
+; Function prints phrase "Access granted" and ends program
+; Enter: none
+; Exit:  none
+; Destr: AX, DX
 ;----------------------------------------------------------
+GrantAccess:    mov ah, 09                              ; DOS Fn 09h - Display text
+                mov dx, offset access_granted_str       ; DX -> access_granted_str
+                int 21h                                 ; print string
+
+                mov ax, 4c00h                           ; DOS Fn 4ch - Terminate
+                int 21h                                 ; Terminate
+;----------------------------------------------------------
+;----------------------------------------------------------
+
+
+
+;----------------------------------------------------------
+; Function prints phrase "Access denied" and ends program
+; Enter: none
+; Exit:  none
+; Destr: AX, DX
+;----------------------------------------------------------
+DenyAccess:     mov ah, 09                              ; DOS Fn 09h - Display text
+                mov dx, offset access_denied_str        ; DX -> access_denied_str
+                int 21h                                 ; print string
+
+                mov ax, 4c00h                           ; DOS Fn 4ch - Terminate
+                int 21h                                 ; Terminate
+;----------------------------------------------------------
+;----------------------------------------------------------
+
+
+
+;----------------------------------------------------------
+; Self defining jump. If the password is correct, it jumps
+; to GrantAccess, if not - to DenyAccess.
+;----------------------------------------------------------
+ChooseAccess:           db 0EBh
+access_function_address dw DenyAccess - ChooseAccess - 2
+;----------------------------------------------------------
+
+;----------------------------------------------------------
+; Password string from user
+;----------------------------------------------------------
+typed_pwd               db PASSWORD_LENGTH dup (0)
+;----------------------------------------------------------
+
+;----------------------------------------------------------
+; Encrypted correct password
+;----------------------------------------------------------
+reference_pwd           db "MeowKitten$"
+;----------------------------------------------------------
+
+
 
 end             Start
